868ff18b3de892d377242b48d1a7698a71620244
zeel0802 <zeel2252@gmail.com>
2024-10-14 17:21:43 -0500

    Implemented generic zip method and added junit

--- src/test/java/com/csc/ZipperTest.java
+++ src/test/java/com/csc/ZipperTest.java
@@ -1,21 +1,41 @@
 package com.csc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class ZipperTest {
 
-  Zipper zipper;
+    @Test
+    public void testZipEqualSizeLists() {
+        List<Integer> list1 = List.of(1, 3, 5);
+        List<Integer> list2 = List.of(2, 4, 6);
+        List<Integer> expected = List.of(1, 2, 3, 4, 5, 6);
+        assertEquals(expected, Zipper.zip(list1, list2));
+    }
 
-  @BeforeEach
-  void setUp() {
-    zipper = new Zipper();
-  }
+    @Test
+    public void testZipFirstListLonger() {
+        List<String> list1 = List.of("Red", "Green", "Blue");
+        List<String> list2 = List.of("White");
+        List<String> expected = List.of("Red", "White", "Green", "Blue");
+        assertEquals(expected, Zipper.zip(list1, list2));
+    }
 
-  @Test
-  void itWorks() {
-    assertEquals(true, true);
-  }
+    @Test
+    public void testZipSecondListLonger() {
+        List<String> list1 = List.of("Red");
+        List<String> list2 = List.of("White", "Black", "Orange");
+        List<String> expected = List.of("Red", "White", "Black", "Orange");
+        assertEquals(expected, Zipper.zip(list1, list2));
+    }
+
+    @Test
+    public void testZipEmptyLists() {
+        List<Integer> list1 = List.of();
+        List<Integer> list2 = List.of();
+        List<Integer> expected = List.of();
+        assertEquals(expected, Zipper.zip(list1, list2));
+    }
 }
